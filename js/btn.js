24320a7bdcaedae94e8658191988a2908428d733
Luke Morgan <luke@Lukes-MacBook-Pro.local>
2018-12-08 14:42:03 +0000

    button functionality update

--- js/btn.js
+++ js/btn.js
@@ -12,3 +12,14 @@ pageBtn.innerHTML="hover";
 
 
 });
+
+pageBtn.addEventListener('click', (e) => {
+e.preventDefault;
+let int = Math.floor(Math.random() * 10);
+const btnColors = ["#9b59b6", "#f1c40f", "#ecf0f1", "#2ecc71", "#e74c3c", "#ff9ff3", "#00d2d3", "#5f27cd", "#48dbfb", "#B33771"];
+
+pageBtn.style=`box-shadow: .1rem 0px 3rem ${btnColors[int]}`; 
+
+
+});
+
