e47f2e5e68692719bdcf78b2db2ab58874e80804
Luke Morgan <luke@Lukes-MacBook-Pro.local>
2018-12-08 15:03:03 +0000

    box shadow tweak

--- js/btn.js
+++ js/btn.js
@@ -3,6 +3,7 @@ let btnContainer = document.querySelector('.button-test__container');
 
 pageBtn.addEventListener('mouseover', (e)=> {
 e.target.innerHTML="click";
+//document.styleSheets[0].insertRule(".button-test__container a:hover { box-shadow: .1rem 0px 3rem rgba(249,249,249,.6); }", 0);
  
 
 });
@@ -22,7 +23,7 @@ e.preventDefault;
 let int = Math.floor(Math.random() * 10);
 const btnColors = ["#9b59b6", "#f1c40f", "#ecf0f1", "#2ecc71", "#e74c3c", "#ff9ff3", "#00d2d3", "#5f27cd", "#48dbfb", "#B33771"];
 
-pageBtn.style=`box-shadow: .1rem 0px 3rem ${btnColors[int]}`; 
+pageBtn.style=`box-shadow: .1rem 0px 4.5rem ${btnColors[int]}`; 
 
 });
 
