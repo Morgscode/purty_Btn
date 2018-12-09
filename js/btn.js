6a41d1d71bc2b38319274da99bbc38fcfdfa6d11
Luke Morgan <luke@Lukes-MacBook-Pro.local>
2018-12-09 08:58:56 +0000

    2nd button

--- js/btn.js
+++ js/btn.js
@@ -1,29 +1,23 @@
-let pageBtn = document.querySelector('.button-test__container a');
-let btnContainer = document.querySelector('.button-test__container');  
-
-pageBtn.addEventListener('mouseover', (e)=> {
-e.target.innerHTML="click";
-//document.styleSheets[0].insertRule(".button-test__container a:hover { box-shadow: .1rem 0px 3rem rgba(249,249,249,.6); }", 0);
+let pageBtn = document.querySelector('.button-hover__container a');
  
 
-});
+pageBtn.addEventListener('mouseover', (e)=> {
+    e.target.innerHTML="click";
+    
+    });
 
 pageBtn.addEventListener('mouseleave', ()=> {
-pageBtn.innerHTML="hover";
-pageBtn.style=`box-shadow: none;`;
-
-
-
+    pageBtn.innerHTML="hover";
+    pageBtn.style=`box-shadow: none;`;
 
 
 });
 
 pageBtn.addEventListener('click', (e) => {
-e.preventDefault;
-let int = Math.floor(Math.random() * 10);
-const btnColors = ["#9b59b6", "#f1c40f", "#ecf0f1", "#2ecc71", "#e74c3c", "#ff9ff3", "#00d2d3", "#5f27cd", "#48dbfb", "#B33771"];
-
-pageBtn.style=`box-shadow: .1rem 0px 4.5rem ${btnColors[int]}`; 
+    e.preventDefault;
+    let int = Math.floor(Math.random() * 10);
+    const btnColors = ["#9b59b6", "#f1c40f", "#ecf0f1", "#2ecc71", "#e74c3c", "#ff9ff3", "#00d2d3", "#5f27cd", "#48dbfb", "#B33771"];
+    pageBtn.style=`box-shadow: .1rem 0px 4.5rem ${btnColors[int]}`; 
 
 });
 
